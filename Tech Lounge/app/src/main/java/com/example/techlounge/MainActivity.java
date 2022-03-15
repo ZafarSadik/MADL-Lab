7beaf3528cc5c591b7bace2a8eecfadf2525ff2c
ZafarSadik <zafarsadik.hd.2019.cse@rajalakshmi.edu.in>
2022-03-15 10:22:43 +0530

    Calculator
    MADL LAB

---
+++ Tech Lounge/app/src/main/java/com/example/techlounge/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.techlounge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
