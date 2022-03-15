9497e89d1d557ec0bb07c08e1e07573b20de5bb6
ZafarSadik <zafarsadik.hd.2019.cse@rajalakshmi.edu.in>
2022-03-15 10:24:04 +0530

    Update MainActivity.java

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -25,13 +25,6 @@ public class MainActivity extends AppCompatActivity {
         n1 = (EditText) findViewById(R.id.n1);
         n2 = (EditText) findViewById(R.id.n2);
         n3 = (EditText) findViewById(R.id.n3);
-
-
-//        num1 = Float.parseFloat(String.valueOf(n1));
-//        num2 = Float.parseFloat(String.valueOf(n2));
-//        num3 = Float.parseFloat(String.valueOf(n3));
-
-
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
